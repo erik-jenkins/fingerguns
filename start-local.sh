b5b970ba5420aedd22fa85843e11d5ce905b2e70
Erik Jenkins <ejenkins132@gmail.com>
2020-12-19 18:16:35 -0500

    use mariadb instead of postgres

---
+++ start-local.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# start from root directory
+cd "${0%/*}"
+
+# generate certs
+cd common/scripts
+./generate-self-signed-cert.sh
+cd ../../FingergunsApi
+./generate-development-cert.sh
+cd ..
+
+# run app in development mode
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build db mariadb adminer client
